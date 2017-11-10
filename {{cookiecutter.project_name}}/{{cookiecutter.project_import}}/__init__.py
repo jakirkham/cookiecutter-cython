bec86ebc9498c199dfbb1884b22749c9811e20a2
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-11-10 12:19:28 -0500

    Remove Python project module
    This will be replaced by the equivalent in Cython form in the `src`
    directory. So go ahead and drop this module as it won't be used any
    more.

--- {{cookiecutter.project_name}}/{{cookiecutter.project_import}}/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = """{{ cookiecutter.full_name }}"""
-__email__ = "{{ cookiecutter.email }}"
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
